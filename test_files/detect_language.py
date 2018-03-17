aaecdac852aad2270d78e00cd84d087823a4cb93
ChocolateSwan <olyasur1996@gmale.com>
2018-03-17 18:08:31 +0300

    hello world flask

---
+++ test_files/detect_language.py
@@ -0,0 +1,16 @@
+# from langdetect import detect
+# from langdetect import detect_langs
+# print(detect_langs('Ты говоришь на "мові"'))
+
+
+import cld2
+
+# текст на Русском
+details = cld2.detect("<title>北京航空航天大学</title>")
+print(str(details))
+
+# Вывод:
+# Detections(is_reliable=True, bytes_found=43, details=(Detection(language_name='RUSSIAN', language_code='ru', percent=97, score=658.0), Detection(language_name='Unknown', language_code='un', percent=0, score=0.0), Detection(language_name='Unknown', language_code='un', percent=0, score=0.0)))
+
+# текст на Украинском
+# details = cld2.detect("Це мій зразок тексту")
