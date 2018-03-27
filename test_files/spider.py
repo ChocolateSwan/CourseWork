e7087e2eebee13742ca1b2c006fc146f38781010
ChocolateSwan <olyasur1996@gmale.com>
2018-03-27 22:27:20 +0300

    link extractor

--- test_files/spider.py
+++ test_files/spider.py
@@ -8,6 +8,7 @@ python3 ./...
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from urllib.parse import urljoin
+from scrapy.linkextractor import LinkExtractor
 import re
 
 
@@ -20,7 +21,7 @@ import re
 class Spider(scrapy.Spider):
     name = "spider"
     # TODO: into params
-    allowed_domains = ['www.ec-lyon.fr']
+    allowed_domains = ['www.uni-stuttgart.de']
     # start_urls = [
     #     'http://www.dmu.ac.uk',
     # ]
@@ -35,56 +36,48 @@ class Spider(scrapy.Spider):
     def parse(self, response):
         print("Current url: ", response.url)
         if response.url not in self.visited_urls:
-            self.visited_urls.append(response.url)
-            # for post_link in response.xpath(
-            #         '//div[@class="post mb-2"]/h2/a/@href').extract():
-            #     url = response.url + post_link
-            #     yield response.follow(url, callback=self.parse_post)
-            # print (response.body)
-            # print(response.xpath('//body').re(r'li.........'))
-            # print(str(re.findall(r'ravoslovnotehn......', response.body)))
-            next_pages = response.xpath(
-                    '//a/@href').extract()
-            # print (next_pages)
-            # next_pages = filter(lambda x: "http" not in x and x not in ['/', '#'], next_pages)
-            # next_pages = map(lambda x:urlparse.urljoin(response.url, x), next_pages )
-            next_pages = self.validate_reference(next_pages, response.url)
-            # print(*next_pages)
-            # next_pages = map(lambda x: self.start_urls[0] + x, next_pages )
-            # next_page = next_pages[1]
-            print(len(next_pages))
-            for url in next_pages:
-                print(url)
-            diff = set(next_pages).difference(self.result_urls)
-            print(len(diff))
+            # Вытаскиваем улры со страницы
+            link_extractor = LinkExtractor()
+            extracted_urls = link_extractor.extract_links(response)
+            extracted_urls = list(map(lambda link: link.url, extracted_urls))
+
+            # Якоря
+            extracted_urls = list(filter(lambda x: x.rfind("#") < x.rfind("/"), extracted_urls))
+            extracted_urls = list(filter(lambda x: self.allowed_domains[0] in x, extracted_urls))
+            # query string
+            extracted_urls = list(map(lambda x: x[0:x.rfind("?")] if x.rfind("?") > x.rfind("/") else x, extracted_urls))
+
+            # study in url!!!!!!!!!!!!!!!!!!
+            # extracted_urls = list(filter(lambda x: "study" in x, extracted_urls))
+
+            # TODO query string, lowercase
+            print(len(extracted_urls))
+            # for url in extracted_urls:
+            #     print(url)
+
+            diff = set(extracted_urls).difference(self.result_urls)
+
             self.result_urls.update(diff)
+
+            print(len(self.result_urls))
+
+            print (len(diff))
 
             for url in diff:
                 print(url)
-            print(len(self.result_urls))
-            # yield response.follow(next_pages[0], callback=self.parse)
 
-            mas_diff = list (diff)
-            # for url in mas_diff:
-            #     print ("qwertyuiop", url)
-            #     yield response.follow(url, callback=self.parse)
 
-            # next_page_url = + next_page
-            # yield response.follow(next_page_url, callback=self.parse)
+            for url in list(diff):
+                yield response.follow(url, callback=self.parse)
 
-    def validate_reference(self, ref_list, url):
-        ref_list = filter(lambda x: x not in ['/', '#'], ref_list)
-        ref_list = map(lambda x: urljoin(url, x) if x[0:4] != "http" else x, ref_list)
-        ref_list = filter(lambda x:self.val(x, url), ref_list)
-        return list(ref_list)
-
-    def val(self, ref, url):
-        return self.allowed_domains[0] in ref and ref not in self.result_urls
 
 
 process = CrawlerProcess({
+    "CONCURRENT_REQUESTS": 100,
+    "REACTOR_THREADPOOL_MAXSIZE": 20,
+    'LOG_LEVEL': 'INFO',
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
 
-process.crawl(Spider, start_url ="http://www.ec-lyon.fr")
+process.crawl(Spider, start_url ="https://www.uni-stuttgart.de")
 process.start()
