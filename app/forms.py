4a5ffaa45d6ff31cc048f39ae5ac24d887550227
ChocolateSwan <olyasur1996@gmale.com>
2018-03-29 12:59:19 +0300

    лажовая страница

--- app/forms.py
+++ app/forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     search = StringField('слова для поиска', validators=[DataRequired("Не пустое")],
-                         render_kw={"class": "form-input"})
+                         render_kw={"class": "form-input form-input-width-100"})
     # TODO choices as list
     select_url = SelectField(u'выбрать сайт для поиска',
                              choices=[('yandex', 'yandex.ru'), ('google', 'google.com'), ('rambler', 'rambler.ru')],
                              validators=[DataRequired("Не пустое")])
-    another_site_flag = BooleanField('сайт не из списка',render_kw={"class": "checkbox-hidden"}
+    another_site_flag = BooleanField('другой сайт',render_kw={"class": "checkbox-hidden"}
                                      )
-    another_site = StringField('другой сайт', validators=[DataRequired()],
-                               render_kw={"class": "form-input"})#, render_kw={'disabled':''},
+    another_site = StringField('сайт не из списка', validators=[DataRequired()],
+                               render_kw={"class": "form-input form-input-width-50"})#, render_kw={'disabled':''},
     search_btn = SubmitField('Искать', render_kw={"class": "form-btn"})
--- app/static/form_control.js
+++ app/static/form_control.js
@@ -6,3 +6,6 @@ another_site_flag.onchange = function() {
     another_site.disabled = !this.checked;
     select_url.disabled = !!this.checked;
 };
+
+//TODO возможно чистить поля после disable, но это не точно
+//TODO лажает при перезагрузке
