1ebc28ee234121ca1e5abef980cddb4bf595cc6e
ChocolateSwan <olyasur1996@gmale.com>
2018-03-29 12:02:18 +0300

    js v1

--- app/static/form_control.js
+++ app/static/form_control.js
@@ -1,11 +1,8 @@
 const another_site_flag = document.getElementById('another_site_flag');
 const another_site = document.getElementById('another_site');
 const select_url = document.getElementById('select_url');
+another_site.disabled = true;
 another_site_flag.onchange = function() {
-  another_site.disabled = !this.checked;
-  if (this.checked) {
-        select_url.attr('disabled', 'disabled');
-    } else {
-        select_url.removeAttr('disabled');
-    }
+    another_site.disabled = !this.checked;
+    select_url.disabled = !!this.checked;
 };
