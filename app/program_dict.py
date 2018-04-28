6f91ee94479a0e54908ad02adb8b2494f5d123c1
ChocolateSwan <olyasur1996@gmale.com>
2018-04-28 23:42:42 +0300

    дополнила список программ

--- app/program_dict.py
+++ app/program_dict.py
@@ -1,17 +1,66 @@
 PROGRAMS = [
     {
         "url": "http://www.dmu.ac.uk/",
-        "название": "Университет де Монфорт",
+        "название": "Университет де Монфорт (Великобритания)",
         "страна": "Великобритания",
         "язык": "en",
         "программы":"http://www.dmu.ac.uk/study/courses/postgraduate-courses"
     },
-        {
+    {
         "url": "https://www.glyndwr.ac.uk",
-        "название": "Университет Глиндер",
+        "название": "Университет Глиндер (Великобритания)",
         "страна": "Великобритания",
         "язык": "en",
         "программы": "https://www.glyndwr.ac.uk/en/Postgraduatecourses/"
+    },
+    {
+        "url": "http://www.tum.de/",
+        "название": "Технический университет Мюнхена (Германия)",
+        "страна": "Германия",
+        "язык": "en",
+        "программы": "https://www.tum.de/en/studies/degree-programs/"
+    },
+    {
+        "url": "https://www.uni-stuttgart.de/",
+        "название": "Университет Штуттгарта (Германия)",
+        "страна": "Германия",
+        "язык": "en",
+        "программы": "https://www.uni-stuttgart.de/en/study/study-programs/program/"
+    },
+    {
+        "url": "http://www.polimi.it/",
+        "название": "Миланский политехнический университет (Италия)",
+        "страна": "Италия",
+        "язык": "en",
+        "программы": "https://www.polimi.it/en/programmes/"
+    },
+    {
+        "url": "https://www.nhtv.nl/ENG/",
+        "название": "Университет БРЕДА прикладных наук (Нидерланды)",
+        "страна": "Нидерланды",
+        "язык": "en",
+        "программы": "https://www.nhtv.nl/ENG/bachelors/"
+    },
+    {
+        "url": "https://www.chalmers.se/en/",
+        "название": "Технический университет Чалмерса (Швеция)",
+        "страна": "Швеция",
+        "язык": "en",
+        "программы": "https://www.chalmers.se/en/education/programmes/"
+    },
+    {
+        "url": "https://www.koreatech.ac.kr/eng/",
+        "название": "Корейский университет технологий и образования - Graduate (Корея)",
+        "страна": "Корея",
+        "язык": "en",
+        "программы": "https://www.koreatech.ac.kr/eng/sub03_02_01.do"
+    },
+    {
+        "url": "https://www.koreatech.ac.kr/eng/",
+        "название": "Корейский университет технологий и образования - Undergraduate (Корея)",
+        "страна": "Корея",
+        "язык": "en",
+        "программы": "https://www.koreatech.ac.kr/eng/sub02_02_01_01.do"
     },
     {
         "url": "http://localhost:5111/documentation/description1.htm",
@@ -26,6 +75,13 @@ PROGRAMS = [
         "страна": "Великобритания",
         "язык": "en",
         "программы": "http://localhost:5111/documentation/description2.htm"
+    },
+    {
+        "url": "http://localhost:5111/documentation/Artist_job.html",
+        "название": "Atrist Word description",
+        "страна": "Великобритания",
+        "язык": "en",
+        "программы": "http://localhost:5111/documentation/Artist_job.html"
     },
 
 ]
